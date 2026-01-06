82cffed5a6e57b5760d68cf5739324c2d7a3a45b
Nathan Fiedler <nathanfiedler@fastmail.fm>
2026-01-05 19:47:06 -0800

    build: add Rust-based docker healthcheck support

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,15 @@ COPY src src/
 RUN cargo build --release
 
 #
+# build the healthcheck binary
+#
+FROM rust:latest AS healthy
+WORKDIR /build
+COPY healthcheck/Cargo.toml .
+COPY healthcheck/src src/
+RUN cargo build --release
+
+#
 # build the final image
 #
 FROM debian:latest
@@ -18,9 +27,12 @@ ARG HOST="0.0.0.0"
 ARG PORT="3000"
 WORKDIR /app
 COPY --from=builder /build/target/release/namazu .
+COPY --from=healthy /build/target/release/healthcheck .
 COPY public public/
 VOLUME /assets
 ENV ASSETS_PATH="/assets/blobstore"
+ENV HEALTHCHECK_PATH="/liveness"
 ENV RUST_LOG="info"
 EXPOSE ${PORT}
+HEALTHCHECK CMD ./healthcheck
 ENTRYPOINT ["./namazu"]
---
+++ healthcheck/src/main.rs
@@ -0,0 +1,27 @@
+use http_body_util::Empty;
+use hyper::Request;
+use hyper::body::Bytes;
+use hyper_util::client::legacy::Client;
+use hyper_util::rt::TokioExecutor;
+use std::env;
+use std::process::exit;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let port = env::var("PORT").unwrap_or("3000".into());
+    let path = env::var("HEALTHCHECK_PATH").unwrap_or("/".into());
+    let url = format!("http://localhost:{port}{path}").parse::<hyper::Uri>()?;
+    let authority = url.authority().unwrap().clone();
+    let req = Request::builder()
+        .uri(url)
+        .method("HEAD")
+        .header(hyper::header::HOST, authority.as_str())
+        .body(Empty::<Bytes>::new())?;
+    let client = Client::builder(TokioExecutor::new()).build_http();
+    let response = client.request(req).await?;
+    let status_code = response.status();
+    if status_code.is_client_error() || status_code.is_server_error() {
+        exit(1)
+    }
+    exit(0)
+}
